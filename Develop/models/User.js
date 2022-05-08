bd100c9c622d282fd4c0a41853377675d65f4d8d
jkstone321 <jkstone321@gmail.com>
2022-05-08 14:13:47 -0400

    index routes for api and controllers done and also initialized user model

--- Develop/models/User.js
+++ Develop/models/User.js
@@ -3,36 +3,48 @@ const bcrypt = require("bcrypt");
 const sequelize = require("../config/config");
 
 class User extends Model {
-    // set up a method to run on instance data (per user) to check passwords
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
 };
 
 User.init(
-    { 
-        // Finish the user model
+    {
         id: {
-            type: DataType.INTEGER,
+            type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
         username: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
         },
         password: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            },
         },
     },
     {
     hooks: {
-        // set up beforeCreate lifecycle "hook"
-        // beforeCreate: async () => {},
-        // beforeUpdate: async () => {}
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          },
     },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscore: true,
-    modelName: "User"
+    modelName: "user"
     }
 );
 
