a5f2c276c71986f1e39f5db639854f5735df416e
jkstone321 <jkstone321@gmail.com>
2022-05-08 14:48:57 -0400

    added comment model

--- Develop/models/Comment.js
+++ Develop/models/Comment.js
@@ -4,7 +4,27 @@ const sequelize = require("../config/config");
 class Comment extends Model {}
 
 Comment.init(
+  {
     // finish this Comment Model
+    comment_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.TEXT,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscore: true,
+    modelName: "comment",
+  }
 );
 
 module.exports = Comment;
